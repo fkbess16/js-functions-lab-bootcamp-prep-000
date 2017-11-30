a7f747b4f3130da58d98efbf40d2d71c076a9713
Flannery Bessette <fkbess16@gmail.com>
2017-11-30 22:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysto(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayto(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown (days,holiday){
+  return `It's ${days} days until ${holiday}!`
+}
